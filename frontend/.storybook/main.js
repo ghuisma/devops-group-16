b5c3a522a57ae6d54929779201aa207874de0003
Matthias van Ingen <matthiasvi@telfort.nl>
2023-02-23 14:29:03 +0100

    storybook with a test button and workflow

---
+++ frontend/.storybook/main.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions" 
+  ],
+  "features": {
+    "interactionsDebugger": true,
+  },
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-webpack5"
+  },
+  "typescript": {
+    "check": false,
+    "checkOptions": {},
+    "reactDocgen": false
+  },
+}
