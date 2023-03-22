809ed90e08d7dc56774c1c639aec5558094bedb4
J. KARELS <jjokarels@gmail.com>
2023-03-22 10:20:35 +0100

    Add register page simple tests

--- frontend/src/stories/register.stories.tsx
+++ frontend/src/stories/register.stories.tsx
@@ -1,5 +1,5 @@
 import { ComponentStory, ComponentMeta } from "@storybook/react";
-import { within, userEvent } from "@storybook/testing-library";
+import { within, userEvent, waitFor } from "@storybook/testing-library";
 import Register from "@/pages/register";
 import { AppBase } from "@/pages/_app";
 import { expect } from "@storybook/jest";
@@ -32,7 +32,7 @@ RendersWithoutError.play = async ({ canvasElement }) => {
     selector: "input",
   });
 
-  await userEvent.type(lastNameInput, "ExamplePassword", {
+  await userEvent.type(lastNameInput, "My Last Name", {
     delay: 100,
   });
 
@@ -73,9 +73,9 @@ RendersWithoutError.play = async ({ canvasElement }) => {
   userEvent.click(submitButton);
 };
 
-export const InputValidationMail = Template.bind({});
+export const ErrorMessageWhenInvalidMail = Template.bind({});
 
-InputValidationMail.play = async ({ canvasElement }) => {
+ErrorMessageWhenInvalidMail.play = async ({ canvasElement }) => {
   const canvas = within(canvasElement);
 
   const firstNameInput = canvas.getByLabelText("First name", {
@@ -90,7 +90,7 @@ InputValidationMail.play = async ({ canvasElement }) => {
     selector: "input",
   });
 
-  await userEvent.type(lastNameInput, "ExamplePassword", {
+  await userEvent.type(lastNameInput, "My Last Name", {
     delay: 100,
   });
 
@@ -128,7 +128,77 @@ InputValidationMail.play = async ({ canvasElement }) => {
 
   const submitButton = canvas.getByRole("button");
 
-  await userEvent.click(submitButton);
+  userEvent.click(submitButton);
 
-  await expect(canvas.getByText("Please enter a valid email")).toBeInTheDocument();
+  await waitFor(
+    () => {
+      expect(canvas.getByText("Please enter a valid email")).toBeInTheDocument();
+    },
+    { timeout: 200 }
+  ); // adjust the timeout value as needed
+};
+
+export const ErrorMessageWhenPasswordMismatch = Template.bind({});
+
+ErrorMessageWhenPasswordMismatch.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+
+  const firstNameInput = canvas.getByLabelText("First name", {
+    selector: "input",
+  });
+
+  await userEvent.type(firstNameInput, "testuser", {
+    delay: 100,
+  });
+
+  const lastNameInput = canvas.getByLabelText("Last name", {
+    selector: "input",
+  });
+
+  await userEvent.type(lastNameInput, "My Last Name", {
+    delay: 100,
+  });
+
+  const emailInput = canvas.getByLabelText("Email", {
+    selector: "input",
+  });
+
+  await userEvent.type(emailInput, "testuser", {
+    delay: 100,
+  });
+
+  const usernameInput = canvas.getByLabelText("Username", {
+    selector: "input",
+  });
+
+  await userEvent.type(usernameInput, "testuser", {
+    delay: 100,
+  });
+
+  const passwordInput = canvas.getByLabelText("Password", {
+    selector: "input",
+  });
+
+  await userEvent.type(passwordInput, "ExamplePassword", {
+    delay: 100,
+  });
+
+  const repeatPasswordInput = canvas.getByLabelText("Repeat password", {
+    selector: "input",
+  });
+
+  await userEvent.type(repeatPasswordInput, "ExamplePasswordMismatch", {
+    delay: 100,
+  });
+
+  const submitButton = canvas.getByRole("button");
+
+  userEvent.click(submitButton);
+
+  await waitFor(
+    () => {
+      expect(canvas.getByText("Please repeat the password")).toBeInTheDocument();
+    },
+    { timeout: 200 }
+  ); // adjust the timeout value as needed
 };
