8c782755fc553d5bd43806460fdb2732ba832cc2
Jonathan Karels <70691290+one-jonathan@users.noreply.github.com>
2023-03-25 13:13:32 +0100

    Update default.test.js

--- e2e/tests/default.test.js
+++ e2e/tests/default.test.js
@@ -28,9 +28,9 @@ test("Answer question", async (t) => {
 
   await t
     .expect(Selector("body").textContent)
-    .contains("Is this a test?", { timeout: 10000 })
+    .contains("Is this a test?", { timeout: 30000 })
     .typeText(answerField, "Yes")
     .click(Selector("button").withText("SUBMIT"))
-    .expect(Selector("body").textContent, { timeout: 10000 })
+    .expect(Selector("body").textContent, { timeout: 30000 })
     .contains("Response has been saved!");
 });
